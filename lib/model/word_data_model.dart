caa1fde25c7c7a200ff7427aa1d95ec8940604fa
wooohyerim <hyerim3414@gmail.com>
2026-02-10 11:13:18 +0900

    fix : word_data > word_data_model 로 변경

--- lib/model/word_data_model.dart
+++ lib/model/word_data_model.dart
@@ -1,8 +1,8 @@
-class WordData {
+class WordDataModel {
   final String text, category, difficulty;
   final int id;
 
-  WordData.fromJson(Map<String, dynamic> json)
+  WordDataModel.fromJson(Map<String, dynamic> json)
     : text = json["text"],
       category = json["category"],
       difficulty = json["difficulty"],
